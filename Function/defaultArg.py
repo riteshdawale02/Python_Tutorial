4dd19e3a760680f43911993b8f39ee9e35336468
riteshdawale02 <riteshdawale1596@gmail.com>
2025-07-24 20:54:59 +0530

    default argument in function parameter

---
+++ Function/defaultArg.py
@@ -0,0 +1,5 @@
+def addition(x,y = 20)->int:
+    return x + y
+
+add = addition(12,8)
+print(add)
---
+++ Function/returnType.py
@@ -0,0 +1,12 @@
+def evenOdd (x):
+    if(x%2== 0):
+        return "Even"
+    else:
+        return "Odd"
+    
+string = evenOdd(12)
+
+print(type(string))
+print(string)
+
+
